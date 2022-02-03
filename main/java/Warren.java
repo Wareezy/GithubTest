b6d0b2d2683207539e62b03b6576dcaa1dcd420a
Wareezy <warrenjaftha16@gmail.com>
2022-02-03 19:27:41 +0200

    Warren jaftha-final commit

---
+++ main/java/Warren.java
@@ -0,0 +1,34 @@
+public class Warren {
+    private String name;
+    private int A, B;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int A) {
+        this.A = A;
+    }
+
+    public int getB() {
+        return B;
+    }
+
+    public void setB(int B) {
+        this.B = B;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculate{" + "name=" + name + ", A=" + A + ", B=" + B + '}';
+    }
+
+}
