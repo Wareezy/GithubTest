d83e97662958bc26a1754c0dcf49a959a2470061
Wareezy <warrenjaftha16@gmail.com>
2022-02-03 18:56:09 +0200

    219005303-first commit

--- main/java/Jody.java
+++ main/java/Jody.java
@@ -1,3 +1,35 @@
 public class Jody {
 
+    private String name;
+    private int A, B;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getA() {
+        return A;
+    }
+
+    public void setA(int A) {
+        this.A = A;
+    }
+
+    public int getB() {
+        return B;
+    }
+
+    public void setB(int B) {
+        this.B = B;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculate{" + "name=" + name + ", A=" + A + ", B=" + B + '}';
+    }
+
 }
